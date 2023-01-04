fc83bcfadaf1df68762776a6c7d897fa3da2fbbe
Hau Nguyen <Hau.Nguyen@isobar.com>
2023-01-04 15:01:42 +0700

    change config store

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
     defaultLanguageCode: 'EN',
     storeDomain:
       // @ts-ignore
-      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'hydrogen-preview.myshopify.com',
+      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'hydrogen-merkle.myshopify.com',
     storefrontToken:
       // @ts-ignore
       Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN ||
-      '3b580e70970c4528da70c98e097c2fa0',
+      '52f17dc3d66c4891bf0df40a9a6bcb7e',
     privateStorefrontToken:
       // @ts-ignore
       Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
